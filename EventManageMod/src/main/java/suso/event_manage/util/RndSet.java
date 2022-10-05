33712c622afa5d06452cfab8996f23b45a0c1eee
Suso <jesusmosteiro@gmail.com>
2022-10-05 19:16:30 +0200

    Random set

---
+++ EventManageMod/src/main/java/suso/event_manage/util/RndSet.java
@@ -0,0 +1,80 @@
+package suso.event_manage.util;
+
+import java.util.*;
+
+public class RndSet<T> extends AbstractSet<T> {
+    private List<T> l = new LinkedList<>();
+    private Map<T, Integer> m = new HashMap<>();
+    private final Random r = new Random();
+
+    public RndSet() {
+    }
+
+    public RndSet(Set<T> data) {
+        for(T element : data) {
+            l.add(element);
+            m.put(element, m.size());
+        }
+    }
+
+    public RndSet(Collection<T> data) {
+        this.addAll(data);
+    }
+
+    @Override
+    public int size() {
+        return m.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return m.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return m.containsKey(o);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return l.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return l.toArray();
+    }
+
+    @Override
+    public boolean add(T element) {
+        if(m.containsKey(element)) return false;
+
+        l.add(element);
+        m.put(element, m.size());
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object element) {
+        if(!m.containsKey(element)) return false;
+
+        l.remove((int) m.get(element));
+        m.remove(element);
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        l = new LinkedList<>();
+        m = new HashMap<>();
+    }
+
+    public T get(int i) {
+        return l.get(i);
+    }
+
+    public T getRandom() {
+        return l.get(r.nextInt(l.size()));
+    }
+}
