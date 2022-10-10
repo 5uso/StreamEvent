896b3e57afff7976371aa93c1778e1c717b0e49d
Suso <jesusmosteiro@gmail.com>
2022-10-11 00:20:05 +0200

    Language identifiers
    stupid implementation

---
+++ EventBaseMod/src/main/java/suso/event_base/mixin/client/IdentifierMixin.java
@@ -0,0 +1,31 @@
+package suso.event_base.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Language;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(Identifier.class) @Environment(EnvType.CLIENT)
+public abstract class IdentifierMixin {
+    @ModifyVariable(
+            method = "<init>([Ljava/lang/String;)V",
+            at = @At("HEAD"),
+            ordinal = 0,
+            argsOnly = true
+    )
+    private static String[] injectPathLang(String[] id) {
+        if(id[1].contains("/lang/")) {
+            String code = MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode();
+            String effective = (code.startsWith("es"  ) || code.startsWith("gl_")
+                             || code.startsWith("ca_" ) || code.startsWith("eu_")
+                             || code.startsWith("ast_")) ? "es" : "en";
+            id[1] = id[1].replace("/lang/", effective);
+        }
+
+        return id;
+    }
+}
