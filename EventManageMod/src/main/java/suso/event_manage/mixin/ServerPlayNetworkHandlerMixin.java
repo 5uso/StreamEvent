8aa71959557e44fa688d25e2969904b53b8a0b55
Suso <jesus.mosteiro@udc.es>
2022-10-11 19:13:22 +0200

    Prevent flying kick and superspeed
    i guess

--- EventManageMod/src/main/java/suso/event_manage/mixin/ServerPlayNetworkHandlerMixin.java
+++ EventManageMod/src/main/java/suso/event_manage/mixin/ServerPlayNetworkHandlerMixin.java
@@ -3,19 +3,17 @@ package suso.event_manage.mixin;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket.Action;
 import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import suso.event_manage.EventManager;
-
-import java.util.OptionalInt;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerPlayNetworkHandlerMixin {
@@ -53,6 +51,54 @@ public abstract class ServerPlayNetworkHandlerMixin {
     private void interceptRightClick(PlayerInteractItemC2SPacket packet, CallbackInfo ci) {
         Hand hand = packet.getHand();
         ServerPlayerEntity player = this.getPlayer();
-        if(EventManager.getInstance().onPlayerRightClick(player, player.getStackInHand(hand), hand)) ci.cancel();;
+        if(EventManager.getInstance().onPlayerRightClick(player, player.getStackInHand(hand), hand)) ci.cancel();
+    }
+
+    @Redirect(
+            method = "tick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;disconnect(Lnet/minecraft/text/Text;)V",
+                    ordinal = 0
+            )
+    )
+    private void dontKickFlying(ServerPlayNetworkHandler instance, Text reason) {
+        System.out.println("Didn't kick for flying"); //TODO
+    }
+
+    @Redirect(
+            method = "tick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;disconnect(Lnet/minecraft/text/Text;)V",
+                    ordinal = 1
+            )
+    )
+    private void dontKickFlyingVehicle(ServerPlayNetworkHandler instance, Text reason) {
+        System.out.println("Didn't kick for flying vehicle"); //TODO
+    }
+
+    @Redirect(
+            method = "onPlayerMove",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;isHost()Z"
+            )
+    )
+    private boolean dontMoveTooQuickly(ServerPlayNetworkHandler instance) {
+        System.out.println("Didn't snap player back"); //TODO
+        return true;
+    }
+
+    @Redirect(
+            method = "onVehicleMove",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;isHost()Z"
+            )
+    )
+    private boolean dontMoveVehicleTooQuickly(ServerPlayNetworkHandler instance) {
+        System.out.println("Didn't snap player vehicle back"); //TODO
+        return true;
     }
 }
