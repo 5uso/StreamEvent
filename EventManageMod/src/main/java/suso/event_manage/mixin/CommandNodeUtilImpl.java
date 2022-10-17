66e926d3e1f6f88489ba3c7d90881358fae7651d
Suso <jesusmosteiro@gmail.com>
2022-10-17 22:30:44 +0200

    Properly unregister commands

--- EventManageMod/src/main/java/suso/event_manage/mixin/CommandNodeUtilImpl.java
+++ EventManageMod/src/main/java/suso/event_manage/mixin/CommandNodeUtilImpl.java
@@ -1,6 +1,8 @@
 package suso.event_manage.mixin;
 
+import com.mojang.brigadier.tree.ArgumentCommandNode;
 import com.mojang.brigadier.tree.CommandNode;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,8 +14,14 @@ import java.util.Map;
 public class CommandNodeUtilImpl<S> implements ICommandNodeUtil {
     @Shadow(remap = false) @Final private Map<String, CommandNode<S>> children;
 
+    @Shadow @Final private Map<String, LiteralCommandNode<S>> literals;
+
+    @Shadow @Final private Map<String, ArgumentCommandNode<S, ?>> arguments;
+
     @Override
     public void removeChild(String key) {
         this.children.remove(key);
+        this.literals.remove(key);
+        this.arguments.remove(key);
     }
 }
