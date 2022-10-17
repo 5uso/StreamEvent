c13cd499625e357a06f3a25528352fe275e891af
Suso <jesusmosteiro@gmail.com>
2022-10-17 22:29:09 +0200

    Force particle function

--- EventManageMod/src/main/java/suso/event_manage/util/ParticleUtil.java
+++ EventManageMod/src/main/java/suso/event_manage/util/ParticleUtil.java
@@ -1,7 +1,11 @@
 package suso.event_manage.util;
 
+import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
+import net.minecraft.particle.ParticleEffect;
 import net.minecraft.scoreboard.AbstractTeam;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3f;
+import suso.event_manage.EventManager;
 
 import java.awt.*;
 
@@ -16,5 +20,10 @@ public class ParticleUtil {
         v_rgb.scale(1.0f / 255.0f);
 
         return v_rgb;
+    }
+
+    public static <T extends ParticleEffect> void forceParticle(T particle, double x, double y, double z, int count, double deltaX, double deltaY, double deltaZ, double speed) {
+        ParticleS2CPacket p = new ParticleS2CPacket(particle, true, x, y, z, (float)deltaX, (float)deltaY, (float)deltaZ, (float)speed, count);
+        for(ServerPlayerEntity player : EventManager.getInstance().getServer().getOverworld().getPlayers()) player.networkHandler.sendPacket(p);
     }
 }
