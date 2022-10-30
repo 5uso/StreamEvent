09273fc1bb0ac348e2c41dc3c3797f4b69b25ed2
Suso <jesusmosteiro@gmail.com>
2022-10-30 20:21:26 +0100

    Bridge effects

--- EventManageMod/src/main/java/suso/event_manage/state_handlers/primatica/PrimaticaBridgeInstance.java
+++ EventManageMod/src/main/java/suso/event_manage/state_handlers/primatica/PrimaticaBridgeInstance.java
@@ -1,6 +1,8 @@
 package suso.event_manage.state_handlers.primatica;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.particle.DustParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.scoreboard.AbstractTeam;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -8,17 +10,21 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3f;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
 import suso.event_manage.state_handlers.TickableInstance;
+import suso.event_manage.util.ParticleUtil;
 import suso.event_manage.util.SoundUtil;
+
+import java.util.List;
 
 public class PrimaticaBridgeInstance implements TickableInstance {
     private final Vec3d direction;
     private Vec3d position;
 
-    private final World world;
+    private final ServerWorld world;
     private final BlockState block;
+    private final Vec3f color;
 
     private int ticksLeft;
 
@@ -28,12 +34,19 @@ public class PrimaticaBridgeInstance implements TickableInstance {
         AbstractTeam team = owner.getScoreboardTeam();
         Identifier blockId = new Identifier(PrimaticaInfo.getCorrespondingBlock(team == null ? 7 :team.getColor().getColorIndex()));
         this.block = Registry.BLOCK.get(blockId).getDefaultState();
+        this.color = team == null ? Vec3f.ZERO : ParticleUtil.teamColor(team);
 
         this.direction = Vec3d.fromPolar(0.0f, owner.getYaw()).multiply(0.6);
         this.position = owner.getPos().add(0.0, Math.min(0.0, owner.getVelocity().y + 0.05) * 5.0 - 0.8, 0.0);
         this.position = new Vec3d(position.x, Math.floor(position.y) + 0.5, position.z);
 
         this.ticksLeft = 20;
+
+        List<ServerPlayerEntity> players = world.getPlayers();
+        world.spawnParticles(ParticleTypes.SCULK_CHARGE_POP, owner.getX(), owner.getY() + 1.0, owner.getZ(), 50, 0.3, 0.5, 0.3, 0.3);
+        SoundUtil.playSound(players, new Identifier("minecraft:block.grindstone.use"), SoundCategory.BLOCKS, position, 1.0f, 0.5f);
+        SoundUtil.playSound(players, new Identifier("minecraft:block.conduit.activate"), SoundCategory.BLOCKS, position, 1.0f, 0.5f);
+        SoundUtil.playSound(players, new Identifier("minecraft:item.trident.riptide_3"), SoundCategory.BLOCKS, position, 1.0f, 0.5f);
     }
 
     @Override
@@ -44,6 +57,9 @@ public class PrimaticaBridgeInstance implements TickableInstance {
         position = position.add(direction);
         BlockPos pos = new BlockPos(position);
 
+        world.spawnParticles(ParticleTypes.DOLPHIN, position.x, position.y, position.z, 20, 1.0, 0.7, 1.0, 0.5);
+        world.spawnParticles(ParticleTypes.SCULK_CHARGE_POP, position.x, position.y, position.z, 1, 0.5, 0.5, 0.5, 0.1);
+        world.spawnParticles(new DustParticleEffect(color, 1.0f), position.x, position.y + 1.0, position.z, 1, 0.0, 0.0, 0.0, 0.0);
         if(prevPos.equals(pos)) return false;
 
         BlockPos change = pos.subtract(prevPos);
@@ -55,7 +71,9 @@ public class PrimaticaBridgeInstance implements TickableInstance {
 
         if(world.getBlockState(pos).isAir()) {
             world.setBlockState(pos, block);
-            SoundUtil.playSound(((ServerWorld) world).getPlayers(), new Identifier("minecraft:block.scaffolding.place"), SoundCategory.MASTER, position, 1.0f, 1.0f);
+            List<ServerPlayerEntity> players = world.getPlayers();
+            SoundUtil.playSound(players, new Identifier("minecraft:block.scaffolding.place"), SoundCategory.BLOCKS, position, 1.0f, 1.0f);
+            SoundUtil.playSound(players, new Identifier("minecraft:block.amethyst_block.place"), SoundCategory.BLOCKS, position, 1.0f, 1.0f);
         }
 
         return false;
