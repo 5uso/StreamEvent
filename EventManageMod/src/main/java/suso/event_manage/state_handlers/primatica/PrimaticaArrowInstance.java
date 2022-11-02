c93799fcc1c42134f65c4eb8cc373d946dc35985
Suso <jesusmosteiro@gmail.com>
2022-11-02 21:29:11 +0100

    Arrow raycast bounces on EMPs

--- EventManageMod/src/main/java/suso/event_manage/state_handlers/primatica/PrimaticaArrowInstance.java
+++ EventManageMod/src/main/java/suso/event_manage/state_handlers/primatica/PrimaticaArrowInstance.java
@@ -1,11 +1,9 @@
 package suso.event_manage.state_handlers.primatica;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.client.particle.FireworksSparkParticle;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.StringNbtReader;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
-import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.scoreboard.AbstractTeam;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -25,19 +23,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimaticaArrowInstance implements TickableInstance {
+    private static final double STEP_SIZE = 0.1;
+
     private final ServerPlayerEntity owner;
     private final ServerWorld world;
     private final AbstractTeam team;
 
-    private final Vec3d direction;
+    private Vec3d direction;
     private Vec3d position;
+    private Vec3d closestEMP;
 
     public PrimaticaArrowInstance(ServerPlayerEntity owner) {
         this.owner = owner;
         this.world = owner.getWorld();
         this.team = owner.getScoreboardTeam();
 
-        this.direction = Vec3d.fromPolar(owner.getPitch(), owner.getYaw()).multiply(0.1);
+        this.direction = Vec3d.fromPolar(owner.getPitch(), owner.getYaw()).multiply(STEP_SIZE);
         this.position = owner.getEyePos();
 
         List<ServerPlayerEntity> players = world.getPlayers();
@@ -53,12 +54,23 @@ public class PrimaticaArrowInstance implements TickableInstance {
 
     @Override
     public boolean tick() {
-        List<Box> playerHitboxes = new ArrayList<>(world.getServer().getCurrentPlayerCount());
-        world.getPlayers().forEach(player -> {
+        List<ServerPlayerEntity> players = world.getPlayers();
+        List<Box> playerHitboxes = new ArrayList<>(players.size());
+        players.forEach(player -> {
             if(!player.isTeamPlayer(team)) playerHitboxes.add(player.getBoundingBox());
         });
 
+        double empDist = empDistance();
+
         for(int steps = 0; steps < 2000; steps++) {
+            if(empDist < 3.1) {
+                empDist = empDistance();
+                if(empDist < 3.1) {
+                    direction = position.subtract(closestEMP).normalize().multiply(STEP_SIZE);
+                    SoundUtil.playSound(players, new Identifier("minecraft:block.end_portal_frame.fill"), SoundCategory.BLOCKS, position, 3.0f, 0.7f);
+                }
+            }
+
             BlockPos pos = new BlockPos(position);
             if(!world.getBlockState(pos).getCollisionShape(world, pos).isEmpty()) {
                 explode();
@@ -75,6 +87,7 @@ public class PrimaticaArrowInstance implements TickableInstance {
             float speed = Math.max(0.01f, 1.0f - (steps * 0.005f));
             ParticleUtil.forceParticle(ParticleTypes.END_ROD, position.getX(), position.getY(), position.getZ(), 1, 0.0, 0.0, 0.0, speed);
             position = position.add(direction);
+            empDist -= STEP_SIZE;
         }
         return true;
     }
@@ -101,5 +114,17 @@ public class PrimaticaArrowInstance implements TickableInstance {
         } catch (CommandSyntaxException | NullPointerException e) {
             e.printStackTrace();
         }
+    }
+
+    private double empDistance() {
+        double r = 500.0;
+        for (Vec3d p : PrimaticaEMPInstance.positions.keySet()) {
+            double d = p.distanceTo(position);
+            if(d < r) {
+                r = d;
+                closestEMP = p;
+            }
+        }
+        return r;
     }
 }
