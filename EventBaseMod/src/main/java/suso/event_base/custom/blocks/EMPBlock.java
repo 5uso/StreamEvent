bf0e6fe6ef571aa559fded2947e2b8131905b0d1
Suso <jesusmosteiro@gmail.com>
2022-11-06 21:51:25 +0100

    This fixed nothing
    i guess it's still an improvement

--- EventBaseMod/src/main/java/suso/event_base/custom/blocks/EMPBlock.java
+++ EventBaseMod/src/main/java/suso/event_base/custom/blocks/EMPBlock.java
@@ -3,8 +3,6 @@ package suso.event_base.custom.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
