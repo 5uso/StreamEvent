ccfce26a071f9350ff3de25f17e7b3a80f27bec0
Suso <jesusmosteiro@gmail.com>
2023-02-03 21:50:07 +0100

    Remove void sun/moon

--- EventBaseMod/src/main/java/suso/event_base/mixin/client/WorldRendererMixin.java
+++ EventBaseMod/src/main/java/suso/event_base/mixin/client/WorldRendererMixin.java
@@ -5,24 +5,26 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.Framebuffer;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.Matrix4f;
+import org.jetbrains.annotations.Nullable;
 import org.lwjgl.opengl.GL30;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import suso.event_base.client.shader.CustomUniformStore;
 
 @Mixin(WorldRenderer.class) @Environment(EnvType.CLIENT)
 public class WorldRendererMixin {
     @Shadow @Final private MinecraftClient client;
+
+    @Shadow @Nullable private ClientWorld world;
 
     @Inject(
             method = "render",
@@ -44,5 +46,45 @@ public class WorldRendererMixin {
         GlStateManager._glBindFramebuffer(GL30.GL_FRAMEBUFFER, globID);
         GlStateManager._glBindFramebuffer(GL30.GL_READ_FRAMEBUFFER, readID);
         GlStateManager._glBindFramebuffer(GL30.GL_DRAW_FRAMEBUFFER, drawID);
+    }
+
+    @Redirect(
+            method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/BufferRenderer;drawWithShader(Lnet/minecraft/client/render/BufferBuilder$BuiltBuffer;)V",
+                    ordinal = 1
+            )
+    )
+    private void controlSunRender(BufferBuilder.BuiltBuffer buffer) {
+        if(this.world == null) return;
+
+        float angle = this.world.getSkyAngle(0.0f) * 360.0F;
+        if(angle > 120.0f && angle < 240.0f) {
+            buffer.release();
+            return;
+        }
+
+        BufferRenderer.drawWithShader(buffer);
+    }
+
+    @Redirect(
+            method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/BufferRenderer;drawWithShader(Lnet/minecraft/client/render/BufferBuilder$BuiltBuffer;)V",
+                    ordinal = 2
+            )
+    )
+    private void controlMoonRender(BufferBuilder.BuiltBuffer buffer) {
+        if(this.world == null) return;
+
+        float angle = this.world.getSkyAngle(0.0f) * 360.0F;
+        if(angle > 300.0f || angle < 60.0f) {
+            buffer.release();
+            return;
+        }
+
+        BufferRenderer.drawWithShader(buffer);
     }
 }
