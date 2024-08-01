ff815b563bf62344f724f08f32e2a1bff073f350
Suso <jesusmosteiro@gmail.com>
2024-08-01 23:14:11 +0200

    Login networking client 1.21

--- EventBaseMod/src/main/java/suso/event_base/client/ModCheck.java
+++ EventBaseMod/src/main/java/suso/event_base/client/ModCheck.java
@@ -1,7 +1,5 @@
 package suso.event_base.client;
 
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking;
@@ -9,8 +7,8 @@ import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientLoginNetworkHandler;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.PacketCallbacks;
 import net.minecraft.util.Util;
-import suso.event_base.EvtBaseConstants;
 import suso.event_base.custom.network.payloads.LoginCheckPayload;
 
 import java.util.concurrent.CompletableFuture;
@@ -21,10 +19,10 @@ public class ModCheck {
     public static void registerPacketListeners() {
         PayloadTypeRegistry.configurationS2C().register(LoginCheckPayload.ID, LoginCheckPayload.CODEC);
 
-        ClientLoginNetworking.registerGlobalReceiver(EvtBaseConstants.LOGIN_CHECK, ModCheck::handleLoginQuery);
+        ClientLoginNetworking.registerGlobalReceiver(LoginCheckPayload.ID.id(), ModCheck::handleLoginQuery);
     }
 
-    private static CompletableFuture<PacketByteBuf> handleLoginQuery(MinecraftClient client, ClientLoginNetworkHandler handler, PacketByteBuf buf, Consumer<GenericFutureListener<? extends Future<? super Void>>> listenerAdder) {
+    private static CompletableFuture<PacketByteBuf> handleLoginQuery(MinecraftClient client, ClientLoginNetworkHandler handler, PacketByteBuf buf, Consumer<PacketCallbacks> callbacksConsumer) {
         long serverTime = buf.readLong();
         offset = serverTime - Util.getMeasuringTimeMs();
 
