426ee234ac69552bd70c8d0c8f736b80661c7a97
Suso <jesusmosteiro@gmail.com>
2024-08-01 20:44:04 +0200

    Entity registry 1.21 client

--- EventBaseMod/src/main/java/suso/event_base/custom/entities/CustomEntities.java
+++ EventBaseMod/src/main/java/suso/event_base/custom/entities/CustomEntities.java
@@ -2,20 +2,20 @@ package suso.event_base.custom.entities;
 
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.*;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class CustomEntities {
     public static EntityType<PrimaticaOrbEntity> PRIMATICA_ORB;
 
     private static <T extends Entity> EntityType<T> register(String id, EntityType<T> entity) {
-        return Registry.register(Registry.ENTITY_TYPE, id, entity);
+        return Registry.register(Registries.ENTITY_TYPE, id, entity);
     }
 
     public static void register() {
-        PRIMATICA_ORB = register("suso:primatica_orb", FabricEntityTypeBuilder.create(SpawnGroup.MISC, PrimaticaOrbEntity::new).dimensions(EntityDimensions.fixed(1.0f, 1.0f)).disableSaving().build());
+        PRIMATICA_ORB = register("suso:primatica_orb", EntityType.Builder.create(PrimaticaOrbEntity::new, SpawnGroup.MISC).dimensions(1.0f, 1.0f).disableSaving().build());
         EntityRendererRegistry.register(PRIMATICA_ORB, PrimaticaOrbRenderer::new);
         FabricDefaultAttributeRegistry.register(CustomEntities.PRIMATICA_ORB, LivingEntity.createLivingAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 10000.0));
     }
