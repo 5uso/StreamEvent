83d9e3179d82e1db9b4e5468aea3a39c92f25fe0
Suso <jesusmosteiro@gmail.com>
2024-08-02 01:59:47 +0200

    Move chat up 1.21

--- EventBaseMod/src/main/java/suso/event_base/mixin/client/InGameHudMixin.java
+++ EventBaseMod/src/main/java/suso/event_base/mixin/client/InGameHudMixin.java
@@ -1,25 +1,23 @@
 package suso.event_base.mixin.client;
 
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
-    @Shadow private int scaledHeight;
-
     @Inject(
             method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/hud/ChatHud;render(Lnet/minecraft/client/util/math/MatrixStack;I)V"
+                    target = "Lnet/minecraft/client/gui/LayeredDrawer;render(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V"
             )
     )
-    private void offsetChat(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
-        matrices.translate(0.0, 48 - scaledHeight / 2.0f, 0.0);
+    private void offsetChat(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        context.getMatrices().translate(0.0, 48 - context.getScaledWindowHeight() / 2.0f, 0.0);
     }
 }
