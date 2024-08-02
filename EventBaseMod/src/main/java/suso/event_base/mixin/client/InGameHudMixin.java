6fbe44bb0d2ef253f7c31094fbc6c12ee4d28948
Suso <jesusmosteiro@gmail.com>
2024-08-02 23:06:28 +0200

    Fix chat offset

--- EventBaseMod/src/main/java/suso/event_base/mixin/client/InGameHudMixin.java
+++ EventBaseMod/src/main/java/suso/event_base/mixin/client/InGameHudMixin.java
@@ -11,13 +11,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
     @Inject(
-            method = "render",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/LayeredDrawer;render(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V"
-            )
+            method = "renderChat",
+            at = @At(value = "HEAD")
     )
-    private void offsetChat(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        context.getMatrices().translate(0.0, 48 - context.getScaledWindowHeight() / 2.0f, 0.0);
+    private void offsetChat(DrawContext ctx, RenderTickCounter tickCounter, CallbackInfo ci) {
+        ctx.getMatrices().push();
+        ctx.getMatrices().translate(0.0, 48 - ctx.getScaledWindowHeight() / 2.0f, 0.0);
+    }
+
+    @Inject(
+            method = "renderChat",
+            at = @At(value = "TAIL")
+    )
+    private void endOffsetChat(DrawContext ctx, RenderTickCounter tickCounter, CallbackInfo ci) {
+        ctx.getMatrices().pop();
     }
 }
