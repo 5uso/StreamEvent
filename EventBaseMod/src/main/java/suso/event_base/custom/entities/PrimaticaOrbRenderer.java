27b4d19b28283d2e1ef1bc5305ad94fcfdbda0aa
Suso <jesusmosteiro@gmail.com>
2024-08-03 15:09:00 +0200

    Remove primatica orb nameplate

--- EventBaseMod/src/main/java/suso/event_base/custom/entities/PrimaticaOrbRenderer.java
+++ EventBaseMod/src/main/java/suso/event_base/custom/entities/PrimaticaOrbRenderer.java
@@ -45,4 +45,9 @@ public class PrimaticaOrbRenderer extends GeoEntityRenderer<PrimaticaOrbEntity> 
 
         return current;
     }
+
+    @Override
+    public boolean hasLabel(PrimaticaOrbEntity entity) {
+        return false;
+    }
 }
