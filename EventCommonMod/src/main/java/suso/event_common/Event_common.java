72a96b1479cb685de9b6ef51a021e70ea27c0f2b
Suso <jesusmosteiro@gmail.com>
2024-08-11 14:20:43 +0200

    New project structure
    EventRoot hosts three subprojects, with the new EventCommonMod being a dependency of both EventBaseMod and EventManageMod

---
+++ EventCommonMod/src/main/java/suso/event_common/Event_common.java
@@ -0,0 +1,10 @@
+package suso.event_common;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Event_common implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
